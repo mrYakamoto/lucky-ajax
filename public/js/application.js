c7902d7dd058384a940e14a70da088d2e8633844
Mia Lehrer & GregoryW <pair+mllehrer.mryakamoto@devbootcamp.com>
2016-01-05 12:28:02 -0800

    Ajaxify die

--- public/js/application.js
+++ public/js/application.js
@@ -6,4 +6,18 @@ $(document).ready(function () {
   //   3- use jQuery to submit an AJAX post to the form's action
   //   4- when the AJAX post is done, display the new die roll using jQuery
 
+  $( "#die_form" ).submit(function(event){
+    event.preventDefault();
+    var sides = $(this).serialize();
+    $.ajax({
+      method: "POST",
+      url: "/rolls",
+      data: sides
+    })
+    .done(function(roll){
+      $(".die").css("display", "block");
+      $("#die_image span").html(roll);
+      $("#die_form")[0].reset();
+    });
+  });
 });
